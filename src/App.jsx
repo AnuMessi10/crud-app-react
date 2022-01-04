a8e8e58db01c4955a41d749d7a81c82dfec31bf6
AnuMessi10 <anuragdyadav2001@gmail.com>
2022-01-05 00:26:28 +0530

    committing optimal approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { React, useState } from 'react';
 import { TextField, Button, MenuItem } from '@mui/material'
 import AbcIcon from '@mui/icons-material/Abc';
 import EscalatorWarningIcon from '@mui/icons-material/EscalatorWarning';
@@ -10,126 +10,139 @@ import TransgenderIcon from '@mui/icons-material/Transgender';
 import ListItem from './Components/ListItem';
 
 const genderList = [
-  {
-    id: 1,
-    value: 'Male',
-    icon: <MaleIcon />
-  },
-  {
-    id: 2,
-    value: 'Female',
-    icon: <FemaleIcon />
-  },
-  {
-    id: 3,
-    value: 'Transgender',
-    icon: <TransgenderIcon />
-  }
+    {
+        id: 1,
+        value: 'Male',
+        icon: <MaleIcon />
+    },
+    {
+        id: 2,
+        value: 'Female',
+        icon: <FemaleIcon />
+    },
+    {
+        id: 3,
+        value: 'Transgender',
+        icon: <TransgenderIcon />
+    }
 ]
 
 export default function App() {
 
-  const [listItems, setListItems] = useState([]);
+    //LHS list using array in a state
+    const [listItems, setListItems] = useState([]);
 
-  const [buttonText, setButtonText] = useState({
-    display: 'Add',
-    keyValue: -1
-  });
+    //to control the value of the button
+    const [buttonText, setButtonText] = useState("Add");
 
-  const deleteElement = (event) => {
-    event.preventDefault();
-    const ID = event.target.value;
-    setListItems(listItems.filter(listItem => listItem.id !== parseInt(ID)));
-  }
-
-  const pushElement = (event) => {
-    event.preventDefault();
-
-    if (buttonText.display === 'Add') {
-      var lastIndex;
-
-      try {
-        lastIndex = listItems.at(-1).id;
-      } catch (error) {
-        lastIndex = 0;
-      }
-
-      const newElement = {
-        id: lastIndex + 1,
-        name: event.target[0].value,
-        age: event.target[1].value,
-        gender: event.target[2].value
-      }
-
-      setListItems(prevListItems =>
-        [...prevListItems, newElement]
-      )
-    }
-    else {
-      listItems[buttonText.keyValue - 1] = {
-        id: buttonText.keyValue,
-        name: event.target[0].value,
-        age: event.target[1].value,
-        gender: event.target[2].value
-      };
-      setButtonText(() => {
-        return { display: 'Add', keyValue: -1 }
-      })
-    }
-  }
-
-  const [genderValue, setGenderValue] = useState({
-    title: "",
-  });
-
-  const updateElement = (event) => {
-
-    const updateItem = listItems.filter(listItem => listItem.id === parseInt(event.target.value));
-
-    document.getElementById("filled-basic-name").value = updateItem[0].name;
-    document.getElementById("filled-basic-age").value = updateItem[0].age;
-
-    setGenderValue(() => { return { title: updateItem[0].gender } });
-
-    setButtonText(() => {
-      return { display: 'Update', keyValue: updateItem[0].id }
+    //to handle the changes of the form values
+    const [formField, setFormField] = useState({
+        id: "",
+        name: "",
+        age: "",
+        gender: ""
     })
-  }
 
-  return (
-    <div className="App">
-      <div className="list">
-        {
-          listItems.map((item) => {
-            return (
-              <ListItem key={item.id} id={item.id} name={item.name} age={item.age} gender={item.gender} deleteElement={deleteElement} updateElement={updateElement} />
-            )
-          })
+    //on submission of form
+    const pushElement = (event) => {
+        event.preventDefault();
+
+        //Check if operation to be performed is add or update
+        (buttonText === "Add") ? addElement() : updateExistingElement();
+    }
+
+    const addElement = () => {
+
+        //creating new object to be added in the list
+        const newElement = {
+            id: Math.floor(Math.random() * 1000),
+            name: formField.name,
+            age: formField.age,
+            gender: formField.gender
         }
-      </div>
-      <form className='form' onSubmit={pushElement}>
-        <div className="form-name">
-          <AbcIcon id="input-icon" sx={{ fontSize: 40 }} />
-          <TextField id="filled-basic-name" label="Enter your name" variant="filled" required fullWidth placeholder="Joseph Menezes" />
+
+        //adding the object to the list
+        setListItems(prevListItems => [...prevListItems, newElement]);
+    }
+
+    const updateExistingElement = () => {
+
+        //creating object for element to be updated
+        const updatedElement = {
+            id: formField.id,
+            name: formField.name,
+            age: formField.age,
+            gender: formField.gender
+        }
+
+        //updating the element using id as a unique key
+        setListItems(listItems.map(listItem => listItem.id === formField.id ? updatedElement : listItem));
+
+        //Changing value of button
+        setButtonText("Add");
+    }
+
+    //delete element using id as key
+    const deleteElement = (ID) => {
+        setListItems(listItems.filter(listItem => listItem.id !== ID))
+    }
+
+    //set value of form same as value of clicked item
+    const getElement = (ID) => {
+
+        //retrieving the values of the clicked element
+        const elementToBeUpdated = listItems.filter(listItem => listItem.id === ID);
+
+        //updating the input fields of the form
+        setFormField(prevValue => {
+            return { ...prevValue, id: elementToBeUpdated[0].id, name: elementToBeUpdated[0].name, age: elementToBeUpdated[0].age, gender: elementToBeUpdated[0].gender };
+        });
+
+        //changing text of button to update
+        setButtonText("Update");
+    }
+
+    //handling values of all 3 input fields using a single state
+    const handleChange = (event) => {
+        setFormField(prevValue => {
+            return { ...prevValue, [event.target.name]: event.target.value }
+        })
+    }
+
+    return (
+        <div className="App">
+            <div className="list">
+                {
+                    listItems.map((item) => {
+                        return (
+                            <ListItem key={item.id} id={item.id} name={item.name} age={item.age} gender={item.gender} deleteElement={deleteElement} getElement={getElement} />
+                        )
+                    })
+                }
+            </div>
+            <form className='form' onSubmit={pushElement}>
+                <div className="form-name">
+                    <AbcIcon id="input-icon" sx={{ fontSize: 40 }} />
+                    <TextField id="filled-basic-name" name="name" label="Enter your name" variant="filled" required fullWidth placeholder="Joseph Menezes" value={formField.name} onChange={handleChange} />
+                </div>
+                <div className="form-age">
+                    <EscalatorWarningIcon id="input-icon" sx={{ fontSize: 35 }} />
+                    <TextField id="filled-basic-age" name="age" label="Enter your age" type="number" variant="filled" required fullWidth placeholder="15" value={formField.age} onChange={handleChange} />
+                </div>
+                <div className="form-gender">
+                    <WcIcon id="input-icon" sx={{ fontSize: 35 }} />
+                    <TextField id="outlined-select-gender" name="gender" select label="Choose your gender" variant="filled" required fullWidth value={formField.gender} onChange={handleChange}>
+                        {genderList.map((gender) => (
+                            <MenuItem key={gender.id} value={gender.value}>
+                                {gender.icon} {gender.value}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </div>
+                <div className="form-btn">
+                    <Button variant="outlined" type="submit">{buttonText}</Button>
+                </div>
+            </form>
         </div>
-        <div className="form-age">
-          <EscalatorWarningIcon id="input-icon" sx={{ fontSize: 35 }} />
-          <TextField id="filled-basic-age" label="Enter your age" type="number" variant="filled" required fullWidth placeholder="15" />
-        </div>
-        <div className="form-gender">
-          <WcIcon id="input-icon" sx={{ fontSize: 35 }} />
-          <TextField id="outlined-select-gender" select label="Choose your gender" variant="filled" required fullWidth value={genderValue.title} onChange={(event) => setGenderValue(() => { return { title: event.target.value } })}>
-            {genderList.map((gender) => (
-              <MenuItem key={gender.id} value={gender.value}>
-                {gender.icon} {gender.value}
-              </MenuItem>
-            ))}
-          </TextField>
-        </div>
-        <div className="form-btn">
-          <Button variant="outlined" type="submit" value={buttonText.keyValue}>{buttonText.display}</Button>
-        </div>
-      </form>
-    </div>
-  );
+    )
 }
--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -4,17 +4,19 @@ import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function ListItem(props) {
+
     return (
         <div className='list-item'>
             <Paper elevation={8} id="list-element">
                 <div className="list-details">
+                    <p><strong>ID: </strong>{props.id}</p>
                     <p><strong>Name: </strong>{props.name}</p>
                     <p><strong>Age: </strong>{props.age}</p>
                     <p><strong>Gender: </strong>{props.gender}</p>
                 </div>
                 <div className="list-btns">
-                    <Button color="success" variant="contained" startIcon={<AddIcon />} value={props.id} onClick={props.updateElement}>Add It</Button>
-                    <Button color="error" variant="contained" startIcon={<DeleteIcon />} value={props.id} onClick={props.deleteElement}>Delete</Button>
+                    <Button color="success" variant="contained" startIcon={<AddIcon />} onClick={() => props.getElement(props.id)}>Add It</Button>
+                    <Button color="error" variant="contained" startIcon={<DeleteIcon />} onClick={() => props.deleteElement(props.id)}>Delete</Button>
                 </div>
             </Paper>
         </div>
